8635c707c76207fe1d8bd256146ad45834fcdc77
youngduck <dudejr5839@naver.com>
2021-09-15 06:37:29 +0900

    BOJ

---
+++ technique.py
@@ -0,0 +1,16 @@
+#-----출력시
+#배열 a를 print(*a) 로 출력하면 알맹이들만 이쁘게나옴.
+
+#import heapq
+
+#-----import numpy   백준에서 numpy 사용할수없음...
+#numpy.dot() -> 행렬계산 -> 2740.py 참조
+#numpy.ndim() -> n차원행렬의 n반환
+#data=numpy.array(data)
+#print(data.shape) -> n행m열 (n,m) 튜플출력
+
+
+#-----import math
+
+#-----배열관련
+#list(filter(함수,리스트))  -> 리스트에서 원하는 함수를 만족하는 값 여러개(리스트로)반환해줌.
