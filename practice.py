fa6e643dfb1a733cd4105dbd840cb0751e8378e0
youngduck <dudejr5839@naver.com>
2021-12-21 21:20:43 +0900

    BOJ211221

--- practice.py
+++ practice.py
@@ -1,3 +1,24 @@
-data=[4,4,4,4]
 
-print(min(data))
+from collections import deque
+
+graph_list = {1: set([3, 4]),
+              2: set([3, 4, 5]),
+              3: set([1, 5]),
+              4: set([1]),
+              5: set([2, 6]),
+              6: set([3, 5])}
+root_node = 1
+
+def BFS_with_adj_list(graph, root):
+    visited = []
+    queue = deque([root])
+
+    while queue:
+        n = queue.popleft()
+        if n not in visited:
+            visited.append(n)
+            queue += graph[n] - set(visited)
+            print(queue)
+    return visited
+  
+print(BFS_with_adj_list(graph_list, root_node))
