df44a7f940690297d4c1489c44d2659eb6be64e5
youngduck <dudejr5839@naver.com>
2022-01-09 12:03:45 +0900

    Add:Tree

---
+++ Tree/2512.py
@@ -0,0 +1,36 @@
+import sys
+
+#초깃값 설정
+cityNum=int(sys.stdin.readline())
+city=list(map(int,sys.stdin.readline().split()))
+budget=int(sys.stdin.readline())
+budgetList=[]
+
+def abc(city,mid):
+    money=0
+    for i in city:
+        if i<mid:
+            money+=i
+        else:
+            money+=mid
+    return money
+
+#첫번째 조건.
+if sum(city) <budget:
+    result=max(city)
+
+#두번째 조건.
+else:
+    left,right=0,max(city)
+    while left<=right:
+        mid=int((left+right)/2)
+        #람다함수 쓰려했으나 else의 경우를 반환하지못하므로 pass
+        money=abc(city,mid)
+        if money <=budget:
+            budgetList.append(mid)
+            left=mid+1
+        else:
+            right=mid-1
+    result=max(budgetList)
+        
+print(result)
---
+++ practice.py
@@ -0,0 +1,4 @@
+data=[3,4,5,6,7]
+
+print(list(filter(lambda x:x>5,data)))
+print(data)
