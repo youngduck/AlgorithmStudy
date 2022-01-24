f3ce6532acbe3b76781bf669906845e4b5fb137b
youngduck <dudejr5839@naver.com>
2022-01-25 02:40:04 +0900

    Add:Graph Theory & Tree

---
+++ Graph Theory/9372.py
@@ -0,0 +1,7 @@
+import sys
+
+for i in range(int(sys.stdin.readline())):
+    a,b=map(int,sys.stdin.readline().split())
+    for j in range(b):
+        x,y=map(int,sys.stdin.readline().split())
+    print(a-1)
