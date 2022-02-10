c4aaf8de7fed4cb7a575667c3c0949be1a140fc1
youngduck <dudejr5839@naver.com>
2022-02-10 21:52:13 +0900

    Add:Dynamic Programming

---
+++ Implementation/1268.py
@@ -0,0 +1,12 @@
+import sys
+
+student_num=int(sys.stdin.readline())
+
+student_list=[0]*(student_num+1)
+
+#1학년~5학년 모두 1~9반까지 존재
+grade1=[1,2,3,4,5,6,7,8,9]
+grade2=[1,2,3,4,5,6,7,8,9]
+grade3=[1,2,3,4,5,6,7,8,9]
+grade4=[1,2,3,4,5,6,7,8,9]
+grade5=[1,2,3,4,5,6,7,8,9]
