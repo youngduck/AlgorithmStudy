5a2136f7a5f810cd0d1dbb76aebf8157efb098b5
youngduck <dudejr5839@naver.com>
2023-03-26 00:24:24 +0900

    Add DataStructures

--- DataStructures/1158.py
+++ DataStructures/1158.py
@@ -1,24 +1,15 @@
 import sys
+from collections import deque
 
-#초깃값
 n,k=map(int,sys.stdin.readline().split())
-destination=k-1
-data=[]
-result=[]
 
-#배열
-for num in range(1,n+1):
-    data.append(num)
+deq=deque(list(range(1,n+1)))
 
-while(data):
-    result.append(data.pop(destination))
-    if len(data)==0:
-        break
-    destination=(destination+k-1)%(len(data))
-    
-print("<",end='')
-for i in range(len(result)-1):
-    print("%d, " %(result[i]),end='')
-print(result[-1],end='')
-print(">")
-    
+print('<',end='')
+while (deq):
+    deq.rotate(-k)
+    if(len(deq)!=1):
+        print(deq.pop(),end=', ')
+    else:
+        print(deq.pop(),end='')
+print('>',end='')
